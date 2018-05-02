c30487af223cb64396013f707e71583ae9b3834e
3tomcha <tomocha.marika@gmail.com>
2018-05-02 15:18:56 +0900

    dbはいちいち開く必要があるようだ

--- app/src/main/java/site/kobatomo/akushukai/Model/ZenkokuModel.java
+++ app/src/main/java/site/kobatomo/akushukai/Model/ZenkokuModel.java
@@ -12,6 +12,7 @@ import site.kobatomo.akushukai.UserContract;
 
 public class ZenkokuModel {
     Context context;
+//    SQLiteDatabase db;
     public ZenkokuModel(Context context){
         this.context=context;
     }
@@ -19,8 +20,7 @@ public class ZenkokuModel {
 
     public Cursor getall(String index){
         UserOpenHelper userOpenHelper = new UserOpenHelper(context);
-        SQLiteDatabase db = userOpenHelper.getReadableDatabase();
-
+        SQLiteDatabase db = userOpenHelper.getWritableDatabase();
         String query6 = "select * from " + UserContract.Users.TABLE_NAME + " where " + UserContract.Users._ID + "=" + index;
         Cursor cursor6 = db.rawQuery(query6, null);
         return cursor6;
@@ -29,8 +29,16 @@ public class ZenkokuModel {
     public void delete(String index){
         UserOpenHelper userOpenHelper = new UserOpenHelper(context);
         SQLiteDatabase db = userOpenHelper.getWritableDatabase();
-
         String DELETE_EVENT = "DELETE FROM "+ UserContract.Zenkoku.TABLE_NAME+" where "+UserContract.Zenkoku.EVENT_ID+"="+index;
         db.execSQL(DELETE_EVENT);
+    }
+
+    public void update(String busuu,String clicked_id,String member_zenkoku){
+        UserOpenHelper userOpenHelper = new UserOpenHelper(context);
+        SQLiteDatabase db = userOpenHelper.getWritableDatabase();
+        String query1 = "update "+ UserContract.Zenkoku.TABLE_NAME
+                +" set " + UserContract.Zenkoku.BUSUU+" = '"+busuu
+                + "' where " + UserContract.Zenkoku.EVENT_ID + " = '"+clicked_id+"' and "+UserContract.Zenkoku.MEMBER+ " = '"+member_zenkoku+"';";
+        db.execSQL(query1);
     }
 }
